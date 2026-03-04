3c8ed70878793c7e86928b9d6fe787d6d1418ff8
Alexandre34420 <landryalexandre868@gmail.com>
2026-03-04 11:50:14 +0100

    Projet jeux échecs

---
+++ chessgamemanage.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ChessGameManager : MonoBehaviour
+{
+    public LayerMask tileMask;
+    public LayerMask pieceMask;
+
+    private ChessPiece selected;
+    private PieceColor turn = PieceColor.White;
+    private ChessPiece[,] board = new ChessPiece[8, 8];
+
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(ray, out RaycastHit hit, 100f)) return;
+
+        if (((1 << hit.collider.gameObject.layer) & pieceMask) != 0)
+        {
+            ChessPiece p = hit.collider.GetComponent<ChessPiece>();
+            if (p.color == turn) selected = p;
+            return;
+        }
+
+        if (((1 << hit.collider.gameObject.layer) & tileMask) != 0 && selected != null)
+        {
+            Vector3 pos = hit.collider.transform.position;
+            int x = Mathf.RoundToInt(pos.x);
+            int y = Mathf.RoundToInt(pos.z);
+
+            if (board[x, y] == null)
+            {
+                board[selected.boardX, selected.boardY] = null;
+                selected.SetPosition(x, y);
+                board[x, y] = selected;
+
+                turn = (turn == PieceColor.White) ? PieceColor.Black : PieceColor.White;
+                selected = null;
+            }
+        }
+    }
+}
+
---
+++ scriptséchecs.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public enum PieceColor { White, Black }
+public enum PieceType { Pawn, Rook, Knight, Bishop, Queen, King }
+
+public class ChessPiece : MonoBehaviour
+{
+    public PieceColor color;
+    public PieceType type;
+
+    public int boardX;
+    public int boardY;
+
+    public void SetPosition(int x, int y)
+    {
+        boardX = x;
+        boardY = y;
+        transform.position = new Vector3(x, 0, y); // Y=0 pour un plateau à plat
+    }
+}
